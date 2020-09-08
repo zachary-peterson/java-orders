4b6fc8c6e6128ffaa7cedffbd827979188bcf7cf
Zachary <zp.lambda@yahoo.com>
2020-09-08 14:52:20 -0400

    ...

--- src/main/java/com/lambdaschool/com/orders/repositories/AgentsRepository.java
+++ src/main/java/com/lambdaschool/com/orders/repositories/AgentsRepository.java
@@ -3,6 +3,6 @@ package com.lambdaschool.com.orders.repositories;
 import com.lambdaschool.com.orders.models.Agent;
 import org.springframework.data.repository.CrudRepository;
 
-public class AgentsRepository extends CrudRepository<Agent, Long>
+public interface AgentsRepository extends CrudRepository<Agent, Long>
 {
 }
--- src/main/java/com/lambdaschool/com/orders/repositories/PaymentsRepository.java
+++ src/main/java/com/lambdaschool/com/orders/repositories/PaymentsRepository.java
@@ -3,6 +3,6 @@ package com.lambdaschool.com.orders.repositories;
 import com.lambdaschool.com.orders.models.Payment;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PaymentsRepositorye extends CrudRepository<Payment, Long>
+public interface PaymentsRepository extends CrudRepository<Payment, Long>
 {
 }
